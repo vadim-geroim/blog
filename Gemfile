55713b821094bf54f4303ddf8c8765a064252124
vgeroim <vgeroim.qa@gmail.com>
2019-08-24 20:06:53 +0000

    create Article model, form on create and Article Show page

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
+gem 'simple_form'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,31 @@
 class ArticlesController < ApplicationController
+
+
+  def new
+    @article = Article.new
+  end
+
   def index
   end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to article_path(current_article)
+
+  end
+
+  def show
+  end
+
+  private
+
+  helper_method :current_article
+  def current_article
+    @article ||= Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
